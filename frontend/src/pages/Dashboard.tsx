1f5cff1c0ba92b80d66a5a737a94bc7dce3d587f
Carlos Davi <carlos.d4vi@gmail.com>
2024-11-06 22:12:00 -0300

    dashboard fix: NEXT_PUBLIC_BACKEND_API_WS_PROD added

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -34,7 +34,7 @@ const Dashboard = () => {
 
   useEffect(() => {
     // Fetch active chats
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/active-chats`)
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_WS_PROD}/active-chats`)
       .then((res) => res.json())
       .then((data) => {
         // Generate a unique time for each chat
